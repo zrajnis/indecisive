b98f54f238bde2b2c0fb85387eb1f6c94eecb922
zrajnis <zvone.rajnis@gmail.com>
2017-03-07 21:44:22 +0100

    repositioned few files (config files outside src everything else in src)

--- server.js
+++ server.js
@@ -5,16 +5,17 @@ const http = require('http').Server(app);
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
-const routes = require('./routes');
-const indexRoutes = require('./routes/index');
-const userRoutes = require('./routes/user');
-const voteRoutes = require('./routes/vote');
-const dilemmaRoutes = require('./routes/dilemma');
+const routes = require('./src/routes/index');
+const indexRoutes = require('./src/routes/index');
+const userRoutes = require('./src/routes/user');
+const voteRoutes = require('./src/routes/vote');
+const dilemmaRoutes = require('./src/routes/dilemma');
+const errorHandler = require('./src/routes/errorHandler');
 const config = require('./config.js');
 
 app.set('port', process.env.PORT || 3000);
 
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/src/views');
 app.set('view engine', 'pug');
 
 const options={ server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },replset: { socketOptions:
@@ -36,25 +37,8 @@ app.use('/vote', voteRoutes);
 app.use('/user', userRoutes);
 app.use('/dilemma', dilemmaRoutes);
 
-app.use(express.static(path.join(__dirname, 'static')));
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use(express.static(path.join(__dirname, '/src/static')));
+app.use('/', errorHandler);
 
 http.listen(app.get('port'),function() {
   console.log('listening on ' + app.get('port'));
